8cda747205f6538e2f0b4a48ae4b6cdf86af61f8
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2023-03-28 14:51:45 -0300

    Ajustes

--- js/menu-hamburguer.js
+++ js/menu-hamburguer.js
@@ -1,6 +1,30 @@
-const menuHamburguer = document.querySelector(".menu__mobile");
-const navItens = document.querySelector(".nav__itens");
+// const menuHamburguer = document.querySelector(".menu__mobile");
+// const navItens = document.querySelector(".nav__itens");
 
-menuHamburguer.addEventListener("click", () => {
-    navItens.classList.toggle("ativo");
-});
+// menuHamburguer.addEventListener("click", () => {
+//     navItens.classList.toggle("ativo");
+// });
+
+// const backgroundBtnLink = () => {
+//     const btn = document.querySelector(".js-link-menu");
+//     const btnLinks = document.querySelectorAll(".js-link-menu")
+    
+//     btn.addEventListener("click", () => {
+//         btnLinks.forEach((link, index) => {
+//             if(index === 0) {
+//                 link.style.background="#fff";
+//             }
+//         })
+//     })
+
+    
+
+
+    
+
+// }
+
+// backgroundBtnLink();
+
+
+
